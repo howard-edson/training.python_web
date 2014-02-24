e09f6c7e053794cffef4b9fc00df4ebe0628846e
Howard Edson <howard.edson@gmail.com>
2014-02-23 20:27:49 -0800

    Edson homework 7

--- assignments/session07/mysite/myblog/admin.py
+++ assignments/session07/mysite/myblog/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
-from myblog.models import Post
-from myblog.models import Category
+from myblog.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_date', 'modified_date', )
+    list_display = ('title', 'created_date', 'modified_date', 'published_date', 'author', )
+    list_display_links = ('title', )
+    fields = ('title', 'created_date', 'modified_date', 'published_date', 'author')
+    inlines = [CategoryInline, ]
+    list_filter = ('published_date', 'author')
+    search_fields = ['title', ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts', )
+    list_display = ('name','description', )
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
